6f3e50a93a8da43f47817a94b7228d8ebc93c6ce
Pauline <gouleypauline@live.fr>
2020-03-03 22:09:09 +0100

    [PAT-21] initialisation du projet

---
+++ src/test/java/com/devlp/patpatme/PatpatmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlp.patpatme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatpatmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
