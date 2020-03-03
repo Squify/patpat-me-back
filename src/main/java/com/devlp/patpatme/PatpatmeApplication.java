be87178bbf1d0b2bf5c2b1f2c047457a44eb5c5c
Pauline <40692004+Squify@users.noreply.github.com>
2020-03-03 22:09:09 +0100

    [PAT-21] initialisation du projet

---
+++ src/main/java/com/devlp/patpatme/PatpatmeApplication.java
@@ -0,0 +1,13 @@
+package com.devlp.patpatme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatpatmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatpatmeApplication.class, args);
+	}
+
+}
