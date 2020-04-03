8dbb9cffed78b65ece107d0090313a70ab472159
Pauline <40692004+Squify@users.noreply.github.com>
2020-04-03 16:14:11 +0200

    [PAT-6] création d'un évènement avec date et type + récupération types dans front

---
+++ src/main/java/com/devlp/patpatme/dto/event/CreateEventDto.java
@@ -0,0 +1,19 @@
+package com.devlp.patpatme.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class CreateEventDto {
+
+    private String name;
+    private String description;
+    private String localisation;
+    private String date;
+    private String fk_id_type;
+}
---
+++ src/main/java/com/devlp/patpatme/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package com.devlp.patpatme.repository;
+
+import com.devlp.patpatme.entity.EventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<EventEntity, Integer> {
+}
