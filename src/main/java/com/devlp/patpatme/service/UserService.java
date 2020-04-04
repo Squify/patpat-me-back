767e2acbcaa580d58c173824909a372ce7a51280
Lucie <lucie.calem@gmail.com>
2020-04-04 19:29:46 +0200

    add userDTO and method getUserById in controller/service/serviceImpl/repo doesnt works

--- src/main/java/com/devlp/patpatme/service/UserService.java
+++ src/main/java/com/devlp/patpatme/service/UserService.java
@@ -1,11 +1,13 @@
 package com.devlp.patpatme.service;
 
 import com.devlp.patpatme.dto.user.CreateAccountDto;
+import com.devlp.patpatme.dto.user.UserDto;
+import com.devlp.patpatme.entity.UserEntity;
 
 public interface UserService {
 
     void createUser(CreateAccountDto createAccountDto);
-
+    UserEntity getUserById(int id);
     boolean userExistsWithMail(String mail);
     boolean userExistsWithPseudo(String pseudo);
 }
