09e3756543bdf22170014e6a9541707c70459016
Pauline <40692004+Squify@users.noreply.github.com>
2020-04-19 16:41:02 +0200

    [PAT-7] add method to get event by id and return event dto

--- src/main/java/com/devlp/patpatme/service/EventService.java
+++ src/main/java/com/devlp/patpatme/service/EventService.java
@@ -1,10 +1,13 @@
 package com.devlp.patpatme.service;
 
 import com.devlp.patpatme.dto.event.CreateEventDto;
+import com.devlp.patpatme.dto.event.EventDto;
 
 public interface EventService {
 
     void createEvent(CreateEventDto createEventDto);
+
+    EventDto getEventById(Integer eventId);
 
     boolean eventExistsWithName(String name);
 }
