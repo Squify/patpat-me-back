6b5198cd4a85bbcdd66e9f05f781c0457505f837
Lucie <lucie.calem@gmail.com>
2020-05-13 20:47:36 +0200

    add AnimalDto

---
+++ src/main/java/com/devlp/patpatme/dto/animal/AnimalDto.java
@@ -0,0 +1,22 @@
+package com.devlp.patpatme.dto.animal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class AnimalDto {
+        private Integer id;
+        private String name;
+        private String birthday;
+        private String fk_id_type;
+        private String fk_id_gender;
+        private String fk_id_breed;
+        private List<String> tempers;
+    }
+
+}
