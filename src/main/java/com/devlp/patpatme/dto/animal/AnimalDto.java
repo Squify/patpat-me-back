2f3a6e6ef6d095f02ec94024ef15000345385ff4
Lucie <lucie.calem@gmail.com>
2020-05-17 16:13:26 +0200

    work

--- src/main/java/com/devlp/patpatme/dto/animal/AnimalDto.java
+++ src/main/java/com/devlp/patpatme/dto/animal/AnimalDto.java
@@ -1,4 +1,8 @@
 package com.devlp.patpatme.dto.animal;
+import com.devlp.patpatme.entity.AnimalGenderEntity;
+import com.devlp.patpatme.entity.AnimalTypeEntity;
+import com.devlp.patpatme.entity.BreedEntity;
+import com.devlp.patpatme.entity.TemperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +18,9 @@ public class AnimalDto {
         private Integer id;
         private String name;
         private String birthday;
-        private String fk_id_type;
-        private String fk_id_gender;
-        private String fk_id_breed;
-        private List<String> tempers;
+        private AnimalTypeEntity type;
+        private AnimalGenderEntity gender;
+        private BreedEntity breed;
+        private List<TemperEntity> tempers;
 
 }
