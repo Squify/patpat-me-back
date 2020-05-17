5764d741858ab58f517d6da705aa03a659989d77
Lucie <lucie.calem@gmail.com>
2020-05-17 17:48:30 +0200

    fix update function but still having err 500 temper

--- src/main/java/com/devlp/patpatme/repository/AnimalRepository.java
+++ src/main/java/com/devlp/patpatme/repository/AnimalRepository.java
@@ -3,6 +3,9 @@ package com.devlp.patpatme.repository;
 import com.devlp.patpatme.entity.AnimalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+
 public interface AnimalRepository extends JpaRepository<AnimalEntity, Integer> {
     AnimalEntity findOneById(Integer id);
+    AnimalEntity findOneByName(String name);
 }
