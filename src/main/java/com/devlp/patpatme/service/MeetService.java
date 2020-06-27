793ad6f69348041b71214c9021e267e68baef94b
Pauline <gouleypauline@live.fr>
2020-06-27 21:36:49 +0200

    meet page and friends page works

--- src/main/java/com/devlp/patpatme/service/MeetService.java
+++ src/main/java/com/devlp/patpatme/service/MeetService.java
@@ -5,9 +5,11 @@ import com.devlp.patpatme.entity.EventEntity;
 import com.devlp.patpatme.entity.UserEntity;
 
 import java.util.List;
+import java.util.Set;
 
 public interface MeetService {
 
-    List<MetUserDTO> getMetUsers(List<EventEntity> lastEvents, UserEntity currentUser);
+    Set<MetUserDTO> getMetUsers(List<EventEntity> lastEvents, UserEntity currentUser);
+
     void changeFriendRelation(UserEntity currentUser, UserEntity friend);
 }
